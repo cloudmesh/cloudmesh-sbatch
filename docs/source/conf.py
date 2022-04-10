9de12a5f1cebc52439c5b7d9071d2ef5e3566c8d
Gregor von Laszewski <laszewski@gmail.com>
2022-04-10 09:16:38 -0400

    sphnx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,20 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme"
 ]
+# TO BE ADDED
+# http://sbrunner.github.io/sphinx-prompt/
+# http://blockdiag.com/en/blockdiag/sphinxcontrib.html
+# https://github.com/sphinx-contrib/fulltoc
+# evaluate: https://nbsphinx.readthedocs.io/en/latest/
+# https://readthedocs.org/
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +60,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
