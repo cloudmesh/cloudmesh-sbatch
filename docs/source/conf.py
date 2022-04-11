4c9db8805511e819a9a1cc110631dc3079112ba9
Robert Knuuti <rknuu@users.noreply.github.com>
2022-04-11 11:35:09 -0700

    Fixed documentation generation path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 import cloudmesh.sbatch
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../cloudmesh'))
+sys.path.insert(0, os.path.abspath('../../cloudmesh'))
 
 
 # -- Project information -----------------------------------------------------
